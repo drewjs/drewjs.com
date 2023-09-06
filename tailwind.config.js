d92faff1eaca457f995af0f18d003fabf9b3d60f
Andrew Stalker <andrew@bitcoinira.com>
2023-09-05 22:02:12 -0700

    formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
-export default { 
+export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Atkinson', ...defaultTheme.fontFamily.sans],
-      }
+      },
     },
   },
   plugins: [],
