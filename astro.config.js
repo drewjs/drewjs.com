c7384d0e2f8a0bb63d6a7a6d357ebde96e4989bf
Andrew Stalker <andrew@bitcoinira.com>
2023-09-28 15:56:45 -0700

    vercel analytics

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/static";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://drewjs.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-})
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    }
+  })
+});
